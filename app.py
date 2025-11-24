33232b6cb820a6b2438be1289cc312f8ed6d763e
imTanush02 <tanushsingh843@ggmail.com>
2025-11-24 17:56:16 +0530

    changes on master2

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ collection = db['flask-tutorial']
 @app.route('/')
 def home():
     current_day_name = datetime.now().strftime('%A')
-     current_date = datetime.now().strftime('%Y-%m-%d') 
+    current_date = datetime.now().strftime('%Y-%m-%d') 
     current_time = datetime.now().strftime('%M:%M:%S')
     return render_template('index.html',current_day_name=current_day_name,current_time=current_time)
 
